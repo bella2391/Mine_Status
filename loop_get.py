152cac58dcdf68b85f31d02b9643902b61080b61
bella2391 <145388572+bella2391@users.noreply.github.com>
2024-06-12 14:16:41 +0900

    Update loop_get.py

--- loop_get.py
+++ loop_get.py
@@ -10,10 +10,10 @@ while True:
    conn = None
    try:
       conn = mysql.connector.connect(
-         host=os.getenv("MYSQL_HOST"),
-         user=os.getenv("MYSQL_USER"),
-         password=os.getenv("MYSQL_PASS"),
-         database=os.getenv("MYSQL_DATABASE")
+         host=os.getenv("DB_HOST"),
+         user=os.getenv("DB_USER"),
+         password=os.getenv("DB_PASS"),
+         database=os.getenv("DB_NAME"),
       )
 
       if conn.is_connected:
@@ -30,34 +30,29 @@ while True:
       online = ""
       online += "オンラインサーバー:\n"
       offline = "オフラインサーバー:\n"
-      servers.append(dict(name="Velocity",port="25565"))
-      servers.append(dict(name="Home",port="25564"))
-      servers.append(dict(name="Latest",port="25577"))
-      servers.append(dict(name="RLCraft",port="25573"))
-      servers.append(dict(name="Gun",port="25567"))
-      servers.append(dict(name="Multi_Isekai",port="25571"))
-      servers.append(dict(name="DungeonF",port="25587"))
-      #print("A")
+      servers.append(dict(name="Velocity",port=os.getenv("Velocity_SERVER_PORT")))
+      servers.append(dict(name="Home",port=os.getenv("Home_SERVER_PORT")))
+      servers.append(dict(name="Latest",port=os.getenv("Latest_SERVER_PORT")))
+      servers.append(dict(name="RLCraft",port=os.getenv("RLCraft_SERVER_PORT")))
+      servers.append(dict(name="Gun",port=os.getenv("Gun_SERVER_PORT")))
+      servers.append(dict(name="Multi_Isekai",port=os.getenv("Multi_Isekai_SERVER_PORT")))
+      servers.append(dict(name="DungeonF",port=os.getenv("DungeonF_SERVER_PORT")))
       stmt=f"SHOW COLUMNS FROM mine_status;"
       cursor.execute(stmt)
       columns = cursor.fetchall()
       conn.commit()
-      #print(columns)
       i=0
       while i<len(columns):
          dbcolumns_list.append(columns[i][0])
          i=i+1
-      #print(dbcolumns_list)
       columns_list = []
       i=0
-      #print("B")
       while i<len(servers):
          columns_list.append(servers[i]["name"])
          i=i+1
-      #print("C")
       i=0
       while i<len(servers):
-         code = f"ms_{servers[i]["name"]} = minestat.MineStat('127.0.0.1',{servers[i]['port']})"
+         code = f"ms_{servers[i]["name"]} = minestat.MineStat('{os.getenv("HOST")}',{servers[i]['port']})"
          exec(code)
          i=i+1
       i=0
@@ -96,12 +91,11 @@ while True:
                cursor.execute(stmt)
                conn.commit()
          i=i+1
-      #print("D")
       print(online)
+      print(offline)
       dt_now = datetime.datetime.now()
       print(dt_now.strftime('%Y年%m月%d日 %H:%M:%S'))
       stmt = (f'''UPDATE mine_status SET {status} WHERE id={1};''')
-      #print(stmt)
       cursor.execute(stmt)
       conn.commit()
       cursor.close()
