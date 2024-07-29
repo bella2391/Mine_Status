c3bb9c18116706ac67ba91edb798eb4c2df36c75
bella2391 <145388572+bella2391@users.noreply.github.com>
2024-07-30 06:21:16 +0900

    update

--- loop_reflect.py
+++ loop_reflect.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import asyncio
 from dotenv import load_dotenv
 import discord
 from discord.ext import tasks
@@ -12,75 +14,77 @@ client = discord.Client(intents=discord.Intents.all())
 tree = app_commands.CommandTree(client)
 
 
-@tasks.loop(seconds = 10)
+@tasks.loop(seconds = 20)
 async def myLoop():
-   conn = None
-   try:
-      conn = mysql.connector.connect(
-         host=os.getenv("DB_HOST"),
-         user=os.getenv("DB_USER"),
-         password=os.getenv("DB_PASS"),
-         database=os.getenv("DB_NAME")
-      )
+    conn = None
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv("DB_NAME")
+        )
 
-      if conn.is_connected:
-         pass
-      if not conn.is_connected():
-         raise Exception("データベースサーバへの接続に失敗しました")
-      embed = discord.Embed(title=f"Server Status", description="", color=discord.Color.green())
-      cursor = conn.cursor()
+        if conn.is_connected:
+            pass
+        if not conn.is_connected():
+            raise Exception("データベースサーバへの接続に失敗しました")
+        embed = discord.Embed(title=f"Server Status", description="", color=discord.Color.green())
+        cursor = conn.cursor(dictionary=True)
+        onlineservers = []
+        maintenance = False
 
-      servers_str = ""
-      players_str = ""
-      playernums_str = ""
-      columns_list = []
-      servers = []
-      servers.append(dict(name="Server1",port=os.getenv("Server1_SERVER_PORT")))
-      servers.append(dict(name="Server2",port=os.getenv("Server2_SERVER_PORT")))
-      
-      i=0
-      while i<len(servers):
-        servers_str += f"{servers[i]["name"]},"
-        players_str += f"{servers[i]["name"]}_player_list,"
-        playernums_str += f"{servers[i]["name"]}_current_players,"
-        columns_list.append(servers[i]["name"])
-        i=i+1
-      servers_str = servers_str[:-1]
-      players_str = players_str[:-1]
-      playernums_str = playernums_str[:-1]
-      stmt=f"SELECT {servers_str} FROM mine_status WHERE id={1};"
-      cursor.execute(stmt)
-      onoffs = cursor.fetchall()
-      conn.commit()
-      stmt=f"SELECT {players_str} FROM mine_status WHERE id={1};"
-      cursor.execute(stmt)
-      players = cursor.fetchall()
-      conn.commit()
-      stmt=f"SELECT {playernums_str} FROM mine_status WHERE id={1};"
-      cursor.execute(stmt)
-      playernums = cursor.fetchall()
-      conn.commit()
-      for name, onoff, player,playernum in zip(columns_list,onoffs[0],players[0],playernums[0]):
-        if int(onoff) == 1:
-          if player == "":
-            player = "No Player"
-          embed.add_field(name=":green_circle:%s" % (name),value="%s/%s: %s" % (playernum,10,player),inline=False)
-      status_channel = client.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
-      status_message = await status_channel.fetch_message(int(os.getenv("DISCORD_MSG_ID")))
-      await status_message.edit(embed=embed)
-      cursor.close()
-   except Exception as e:
-      print(f"Error Occurred: {e}")
+        stmt = "SELECT * FROM mine_status;"
+        cursor.execute(stmt)
+        status = cursor.fetchall()
+        #print(status)
 
-   finally:
-      if conn is not None and conn.is_connected():
-         conn.close()
+        for row in status:
+            name = row["name"]
+            online = row["online"]
+            if online:
+                # MaintenanceのonlineカラムがTrueなら
+                if name == "Maintenance":
+                    maintenance = True
+                    break
+                player_list = row["player_list"]
+                current_players = row["current_players"]
+                onlineservers.append(dict(name=name, player_list=player_list, current_players=current_players))
+
+        if maintenance:
+            embed.add_field(name=":red_circle:%s" % ("現在サーバーメンテナンス中"), value="", inline=False)
+            status_channel = client.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
+            status_message = await status_channel.fetch_message(int(os.getenv("DISCORD_MSG_ID")))
+            await status_message.edit(embed=embed)
+            cursor.close()
+            # Maintenance 状態の場合、10秒待機してからループの最初に戻る
+            await asyncio.sleep(10)
+            return  # ここでループの次の実行まで待機
+        
+        for server in onlineservers:
+            name = server["name"]
+            player_list = server["player_list"]
+            current_players = server["current_players"]
+            if player_list == "" or player_list == "NONE":
+                player_list = "No Player"
+            embed.add_field(name=":green_circle:%s" % (name), value="%s/%s: %s" % (current_players ,10, player_list), inline=False)
+
+        status_channel = client.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
+        status_message = await status_channel.fetch_message(int(os.getenv("DISCORD_MSG_ID")))
+        await status_message.edit(embed=embed)
+        cursor.close()
+    except Exception as e:
+        print(f"Error Occurred: {e}")
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
 
 @client.event
 async def on_ready():
- await client.change_presence(activity=discord.Game(name="Status Updating..."))
- await tree.sync()
- print("login complete")
- myLoop.start()
+    await client.change_presence(activity=discord.Game(name="Status Updating..."))
+    await tree.sync()
+    print("login complete")
+    myLoop.start()
 
 client.run(TOKEN)
